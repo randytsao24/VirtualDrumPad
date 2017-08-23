c677972958555b5ace8a69ce48494158b3a6471b
Randy Tsao <randytsao24@gmail.com>
2017-08-23 13:27:04 -0400

    Removed code for SoundPool implementation, will stick to using MediaPlayer resources for now.

--- app/src/main/java/com/randytsao/virtualdrumpad/VirtualPadDrumPad.java
+++ app/src/main/java/com/randytsao/virtualdrumpad/VirtualPadDrumPad.java
@@ -39,40 +39,13 @@ public class VirtualPadDrumPad {
     // Constructor
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public VirtualPadDrumPad(Button b) {
-        isLoaded = false;
-
         padButton = b;
-
-        // *.Builder() only applicable to APIs w/ version 21 or above
-        if (Build.VERSION.SDK_INT >= 21) {
-            // Initialize audio attributes for sound pool
-            sampleAttributes = new AudioAttributes.Builder()
-                    .setUsage(AudioAttributes.USAGE_MEDIA)
-                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                    .build();
-
-            soundPool = new SoundPool.Builder()
-                    .setAudioAttributes(sampleAttributes)
-                    .build();
-        } else {
-            soundPool = new SoundPool(8, AudioManager.STREAM_MUSIC, 0);
-        }
-
-        //soundPool.setVolume();
     }
 
     // setSample()
     // set sample to pad's sample ID
     public void setSample(int requestedId) {
         sampleRawId = requestedId;
-
-        // check whether or not soundPool load has completed or not
-        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
-            @Override
-            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
-                isLoaded = true;
-            }
-        });
     }
 
     // loadSample()
